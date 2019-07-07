e803d2c7ca18dc359ebc95024f7445c346a09091
simpart <simpart>
2019-07-07 17:54:11 +0900

    add targetKeys

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # mofron-event-style
-style change event for mofron component
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+style change event for mofron
+
+event function is called when the component style is changed
+
+## Feature
+ - the target style keys can be specified by the "targetKeys" parameter
+ - all style keys are a target if "targetKeys" parameters are null
+ - you can select whether to make execute handler when a value is already set when this event is registered to the component
+## Attention
+ - for valid this event, style changing must be from the mofron API (ex. component.style()).
+
+# Install
+```
+npm install mofron mofron-event-style
+```
+
+# Sample
+```html
+<require>
+    <tag module="mofron-comp-frame">Frame</tag>
+    <tag module="mofron-event-style">evStyle</tag>
+</require>
+
+<script run=init>
+let evt = (p1,p2) => { console.log(p2); }
+</script>
+<script>
+frm.width("1.5rem");
+</script>
+
+<Frame name=frm event=evStyle(evt,"width")>
+</Frame>
+```
+# Parameter
+
+|Simple<br>Param | Parameter Name | Type | Description |
+|:--------------:|:---------------|:-----|:------------|
+| | initEvent | boolean | true: if the target key value is already set, the handler is executed. |
+| | | | false: if the target key value is already set, the handler is not executed. |
+| ◯  | handler |  | handler parameter |
+| ◯  | targetKeys | string/array | watching target keys |
+
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /**
  * @file mofron-event-style/index.js
  * @brief style change event for mofron
+ *        event function is called when the component style is changed
+ * @feature the target style keys can be specified by the "targetKeys" parameter
+ *          all style keys are a target if "targetKeys" parameters are null
+ *          you can select whether to make execute handler when a value is already set when this event is registered to the component
+ * @attention for valid this event, style changing must be from the mofron API (ex. component.style()).
  * @author simpart
  */
 const mf = require('mofron');
@@ -8,15 +13,17 @@ mf.event.Style = class extends mf.Event {
     /**
      * initialize event
      *
-     * @param p1 (object) event option
-     * @param p1 (array)  event handler
-     * @param p2 (string) watching target
+     * @param (array/object) array: event handler [function,mixed]
+     *                       object:  event option
+     * @param (string/array) watching target style keys
+     * @pmap handler,targetKeys
+     * @type private
      */
     constructor (po, p2) {
         try {
             super();
-            this.name('Style');
-            this.prmMap(['handler', 'target']);
+            this.name("Style");
+            this.prmMap(["handler", "targetKeys"]);
             this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
@@ -27,27 +34,45 @@ mf.event.Style = class extends mf.Event {
     /**
      * set style listener
      *
-     * @note private method
+     * @type private
      */
     contents (tgt_dom) {
         try {
-            if (null === this.target()) {
-                console.warn('could not find watching target');
-                return;
-            }
             let fnc = (p1, p2, p3) => {
-                try { p3.execHandler(p1); } catch (e) {
+                try { p3.execHandler(p2); } catch (e) {
                     console.error(e.stack);
                     throw e;
                 }
             }
-            tgt_dom.styleListener(this.target(), fnc, this);
-            
-            let tgt_stl = tgt_dom.style(this.target());
-            if (null !== tgt_stl) {
-                let sty = {};
-                sty[this.target()] = tgt_stl;
-                this.execHandler(sty);
+            let keys = this.targetKeys();
+            if (0 === keys.length) {
+                tgt_dom.styleListener(null, fnc, this);
+            } else {
+                for (let kidx in keys) {
+                    tgt_dom.styleListener(keys[kidx], fnc, this);
+                }
+            }
+            /* init event */
+            if (true === this.initEvent()) {
+                let dm_st = tgt_dom.style();
+                let tkeys = this.targetKeys();
+                if (0 === tkeys.length) {
+                    for (let dt_idx in dm_st) {
+                        if ( ("" !== dm_st[dt_idx]) && (undefined !== dm_st[dt_idx]) ) {
+                            this.execHandler(dm_st);
+                            break;
+                        }
+                    }
+                } else {
+                    for (let tk_idx in tkeys) {
+                        if ( ("" !== dm_st[tkeys[tk_idx]]) &&
+                             (undefined !== dm_st[tkeys[tk_idx]]) ) {
+                            let ev_prm = {};
+                            ev_prm[tkeys[tk_idx]] = dm_st[tkeys[tk_idx]];
+                            this.execHandler(ev_prm);
+                        }
+                    }
+                }
             }
         } catch (e) {
             console.error(e.stack);
@@ -56,14 +81,30 @@ mf.event.Style = class extends mf.Event {
     }
     
     /**
+     * whether running handler if the value of target keys are already set.
+     *
+     * @param (boolean) true: if the target key value is already set, the handler is executed.
+     *                  false: if the target key value is already set, the handler is not executed.
+     * @return (boolean) init handler flag
+     * @type parameter
+     */
+    initEvent (prm) {
+        try { return this.member("initEvent", "boolean", prm, false); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+
+    }
+    
+    /**
      * watching target setter/getter
      *
-     * @param p1 (string) watching target
-     * @param p1 (undefined) call as getter
-     * @return 
+     * @param (string/array) watching target keys
+     * @return (array) watching target keys
+     * @type parameter
      */
-    target (prm) {
-        try { return this.member('target', 'string', prm); } catch (e) {
+    targetKeys (prm) {
+        try { return this.arrayMember("targetKeys", "string", prm); } catch (e) {
             console.error(e.stack);
             throw e;
         }
